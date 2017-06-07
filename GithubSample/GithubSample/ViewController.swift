4733e186db902da26bd208e4b595ea75a844725e
佐々木直哉 <naoya-sasaki@navitime.co.jp>
2017-06-07 15:06:48 -0700

    Initiali Commit

---
+++ GithubSample/GithubSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GithubSample
+//
+//  Created by 佐々木 直哉 on 2017/06/07.
+//  Copyright © 2017年 佐々木 直哉. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
